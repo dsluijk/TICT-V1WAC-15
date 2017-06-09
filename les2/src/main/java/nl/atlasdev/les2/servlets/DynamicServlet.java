4a874afbfb6346a8ca0f05eb700b513e2ea21596
Dany Sluijk <dany@atlasdev.nl>
2017-06-09 11:47:14 +0200

    add les 2

---
+++ les2/src/main/java/nl/atlasdev/les2/servlets/DynamicServlet.java
@@ -0,0 +1,23 @@
+package nl.atlasdev.les2.servlets;
+
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet(urlPatterns = "/DynamicServlet.do")
+public class DynamicServlet extends HttpServlet {
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String name = req.getParameter("username");
+		PrintWriter out = resp.getWriter();
+		resp.setContentType("text/html");
+		out.println("<!DOCTYPE html>");
+		out.println("<html>");
+		out.println(" <title>Dynamic Example</title>");
+		out.println(" <body>");
+		out.println(" <h2>Dynamic webapplication example</h2>");
+		out.println(" <h2>Hello " + name + "!</h2>");
+		out.println(" </body>");
+		out.println("</html>");
+	}
+}
